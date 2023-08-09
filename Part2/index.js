f0625d215be177e832acc7fcf2ce6ae7c9624142
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-08-09 17:53:27 -0600

    finished

--- Part2/index.js
+++ Part2/index.js
@@ -1,2 +1,13 @@
+console.log("You got this!!")
 
-console.log ("hey")
+
+const idInput = document.querySelector('#idInput')
+//const idInput = document.getElementById("idInput")
+const colorInput = document.querySelector('#colorInput')
+
+
+function setCard() {
+    const card = document.querySelector(`#${idInput.value}`);
+    card.style.color = colorInput.value;
+    console.log(card)
+}
